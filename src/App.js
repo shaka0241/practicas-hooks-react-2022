e4a8aa3295100cc73c2e3e9f542236a0a2fc314c
Alberto Rojas <shaka0241@gmail.com>
2022-09-13 17:35:38 -0300

    hook- useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0)
   const [frutas, cantidadFrutas] = useState(0);
+  const [frutaFavorita, nombreFrutaFavorita] = useState('Banana');
+
+  useEffect(() =>{
+    setContador(contador + 1,console.log('Se monto') )
+    return () => {
+      console.log('Se desmonto')
+    }
+  }, [])
+
   return (
     <div className="App">
       <button onClick={() => cantidadFrutas(frutas + 1)}>Comprar Fruta</button>
       <button onClick={() => cantidadFrutas(frutas - 1)}>Sacar Fruta</button>
       <h1>Total de {frutas} compradas</h1>
+      <p>{frutaFavorita}</p>
+      <h1>{contador}</h1>
     </div>
   );
 }
