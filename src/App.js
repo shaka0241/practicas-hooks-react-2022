00e43e78baac8221a1f98a6333814c59e3f740ce
Alberto Rojas <shaka0241@gmail.com>
2022-09-13 16:56:49 -0300

    primer hook- useSate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [frutas, cantidadFrutas] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => cantidadFrutas(frutas + 1)}>Comprar Fruta</button>
+      <button onClick={() => cantidadFrutas(frutas - 1)}>Sacar Fruta</button>
+      <h1>Total de {frutas} compradas</h1>
     </div>
   );
 }
